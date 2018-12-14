c85d3a1060300f4f451d83f900036f13a99727a3
adadgio <romain.bruckert.esc@gmail.com>
2018-12-14 09:40:37 +0100

    Inlined button component template.

--- components/button/button.component.ts
+++ components/button/button.component.ts
@@ -2,7 +2,17 @@ import { Input, Component } from '@angular/core';
 
 @Component({
     selector: 'app-button',
-    templateUrl: './button.component.html',
+    template: `
+    <button [disabled]="disabled" [attr.id]="id">
+        <span [ngClass]="(!loading) ? 'visible' : 'invisible'"><ng-content></ng-content></span>
+        <ng-container *ngIf="loading">
+            <span class="loader">
+                <img *ngIf="loaderColor === 'white'" class="svg-loader" src="assets/svg-loaders/three-dots.svg" width="20" alt="Loading...">
+                <img *ngIf="loaderColor === 'red'" class="svg-loader" src="assets/svg-loaders/three-dots-red.svg" width="20" alt="Loading...">
+            </span>
+        </ng-container>
+    </button>`,
+    // templateUrl: './button.component.html',
     styleUrls: ['./button.component.scss']
 })
 export class ButtonComponent
